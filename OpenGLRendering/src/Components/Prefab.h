12a5bc1d1ca2619cf30d8cd1b62d605e664e662b
DireVision <yewchong25@live.com>
2024-09-25 14:54:36 +0800

    Some comments and cleanup

--- OpenGLRendering/src/Components/Prefab.h
+++ OpenGLRendering/src/Components/Prefab.h
@@ -1,7 +1,15 @@
 /*
+* \author Yew Chong
   \brief This file contains the function to spawn an entity from a prefab file.
          This function only works within the context of the project, and the using namespace needs to be edited for it to work in other projects.
-  \author Yew Chong
+
+         Usage: Include Prefab.h in the source file where you want to spawn entities from prefab files.
+                EntityID e = SpawnEntityFromPrefab("test") where test is the name of the prefab
+         
+                From there, you can use the entityID handle to manipulate the entity as you wish.
+
+         Note: The prefab creation function is available from the Scene class and can be invoked as follows:
+                FlexECS::Scene::GetActiveScene()->SaveEntityAsPrefab(object.Get() , "test");
 */
 #pragma once
 #include <FlexEngine.h> // Utility functions
@@ -10,9 +18,9 @@
 using namespace OpenGLRendering; // This is solution specific...
 
 /*!
-      \brief Spawns an entity with configurations from a prefab file.
-      \param prefabName Name of the prefab file.
-    */
+  \brief Spawns an entity with configurations from a prefab file.
+  \param prefabName Name of the prefab file.
+*/
 FlexECS::Entity& SpawnEntityFromPrefab(const std::string& prefabName)
 {
   // Open the prefab file
