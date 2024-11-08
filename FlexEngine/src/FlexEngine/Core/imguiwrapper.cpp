3675be657d3ebc7ca67870c8c639aa3a83e3be3b
DireVision <yewchong25@live.com>
2024-11-08 17:40:46 +0800

    IMGUI hardcode
    Fix the dependency on Engine font, this should never be the case as this won't work when deploying the build

--- FlexEngine/src/FlexEngine/Core/imguiwrapper.cpp
+++ FlexEngine/src/FlexEngine/Core/imguiwrapper.cpp
@@ -185,7 +185,9 @@ namespace FlexEngine
     CustomImguiStyle();
 
     // load font
-    io.Fonts->AddFontFromFileTTF("..\\FlexEngine\\assets\\fonts\\Noto_Sans\\static\\NotoSans-Regular.ttf", 21.f);
+    std::string currentDir = std::filesystem::current_path().string();
+    std::string fontPath = currentDir + "/assets/fonts/Suez_One/SuezOne-Regular.ttf";
+    io.Fonts->AddFontFromFileTTF(fontPath.c_str(), 21.f);
 
     // setup platform/renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window->GetGLFWWindow(), true);
